6848582a9af82287239e666b6d7d9fdb9680e0a9
diptomistry <diptomistrybd@gmail.com>
2024-02-23 20:31:12 +0600

    hello

--- backend/app.js
+++ backend/app.js
@@ -1,41 +1,51 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors=require("cors");
+app.use(express.json());//to convert the data to json format,otherwise we cant post data to the server from postman
+app.use(cors());//to allow the client to access the server
+const userRouter = require("./routes/api/users/user.router");
+const pool = require("./routes/database");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
-var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.use("/api/users", userRouter);//to use the userRouter for the /api/users endpoint 
+//This line tells Express to use the userRouter for any requests that start with the path "/api/users". 
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//console.log(process.env);
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.post('/api/users/signup', (req, res) => {
+  const sql='INSERT INTO login (name,email,password) VALUES (?)';
+  const values=[
+    req.body.name,
+    req.body.email,
+    req.body.password
+  ]
+  pool.query(sql,[values],(err,data)=>{
+    if(err){
+      return res.json("Error");
+    }
+    else{
+      return res.json(data);
+    }
+  });
+});
+app.post('/api/users/loginn', (req, res) => {
+  const sql='SELECT * FROM login WHERE email=? AND password=?';
+ 
+  pool.query(sql,[req.body.email,req.body.password],(err,data)=>{
+    if(err){
+      return res.json("Error");
+    }
+    if(data.length>0){
+      return res.json("Success");
+    }
+    else{
+      return res.json('Failed to login');
+    }
+  });
+});
+app.listen(process.env.APP_PORT, () => {
+  console.log("Server has started on port:", process.env.APP_PORT);
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
--- frontend/src/home.jsx
+++ frontend/src/home.jsx
@@ -4,57 +4,175 @@ import React, { useState } from "react";
 import Logo from "./assets/logo.png";
 import MeducareImage from "./assets/meducare.png";
 import styles from "./style";
+import "./css_style/home.css";
+import { medicalCenterContent } from "./constants";
 
 function Home() {
+  const [activeButton, setActiveButton] = useState("about");
 
+  const handleButtonClick = (button) => {
+    setActiveButton(button);
+  };
 
+  const renderContent = () => {
+    const boxStyle = {
+      // Set your desired width
+      height: styles.homeBoxHeight, // Set your desired height
+      marginLeft: "100px",
+      marginRight: "100px",
+      marginTop: "50px",
+      border: "4px solid #1d4ed8", // Set your desired border properties
+      borderRadius: "8px",
+    };
+    switch (activeButton) {
+      case "about":
+        return (
+          <div
+            style={boxStyle}
+            className="bg-white border-gray-200 dark:bg-gray-900 p-8 rounded-lg"
+          >
+            <h2 className={styles.heading2}>{medicalCenterContent[1].title}</h2>
+            <p className={`${styles.paragraph} mb-4`}>
+              {medicalCenterContent[0].paragraph}
+            </p>
+            <p className={styles.paragraph}>
+              {medicalCenterContent[1].paragraph}
+            </p>
+          </div>
+        );
+      case "people":
+        return (
+          <div style={boxStyle} className="bg-gray-200 p-8 rounded-lg">
+            <p>People Section Content</p>
+          </div>
+        );
+      case "contact":
+        return (
+          <div
+            style={boxStyle}
+            className="bg-white border-gray-200 dark:bg-gray-900 p-8 rounded-lg"
+          >
+            <h2 className={styles.heading2}>Contact</h2>
+
+            <p className={styles.paragraph}>
+              {medicalCenterContent[2].paragraph[0]}
+              <br />
+              {medicalCenterContent[2].paragraph[1]}
+              <br />
+              {medicalCenterContent[2].paragraph[2]}
+              <br />
+              {medicalCenterContent[2].paragraph[3]}
+              <br />
+              <br />
+
+              {medicalCenterContent[2].paragraph[4]}
+            </p>
+          </div>
+        );
+      default:
+        return null;
+    }
+  };
   return (
-   
     <div>
-      <nav className={`bg-white border-gray-200 dark:bg-gray-900 ${styles.fadeIn}`}>
+      <nav className={`bg-white border-gray-200 dark:bg-gray-900 `}>
         <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="https://www.du.ac.bd//" class="flex items-center space-x-3 rtl:space-x-reverse">
-         <img src={Logo}  class="h-8" alt="DU" />
-            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">UNIVERSITY OF DHAKA</span>
-        </a>
-        <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-            <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get started</button>
-            <button data-collapse-toggle="navbar-cta" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-cta" aria-expanded="false">
+          <a
+            href="https://www.du.ac.bd//"
+            class="flex items-center space-x-3 rtl:space-x-reverse"
+          >
+            <img src={Logo} class="h-8" alt="DU" />
+            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+              UNIVERSITY OF DHAKA
+            </span>
+          </a>
+          <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+            <button
+              type="button"
+              class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Get started
+            </button>
+            <button
+              data-collapse-toggle="navbar-cta"
+              type="button"
+              class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              aria-controls="navbar-cta"
+              aria-expanded="false"
+            >
               <span class="sr-only">Open main menu</span>
-              <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+              <svg
+                class="w-5 h-5"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 17 14"
+              >
+                <path
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M1 1h15M1 7h15M1 13h15"
+                />
               </svg>
-          </button>
-        </div>
-        <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-cta">
-          <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-            <li>
-              <a href="#" class="block py-2 px-3 md:p-0 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:dark:text-blue-500" aria-current="page">About</a>
-            </li>
-            <li>
-              <a href="#" class="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">People</a>
-            </li>
-            <li>
-              <a href="#" class="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 d:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
-            </li>
-           
-          </ul>
-        </div>
+            </button>
+          </div>
+          <div
+            class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+            id="navbar-cta"
+          >
+            <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+              <li>
+                <a
+                  href="#"
+                  className={`block py-2 px-3 md:p-0 ${
+                    activeButton === "about"
+                      ? "text-blue-700"
+                      : "text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                  }`}
+                  onClick={() => handleButtonClick("about")}
+                  aria-current={activeButton === "about" ? "page" : undefined}
+                >
+                  About
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className={`block py-2 px-3 md:p-0 ${
+                    activeButton === "people"
+                      ? "text-blue-700 "
+                      : "text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                  }`}
+                  onClick={() => handleButtonClick("people")}
+                >
+                  People
+                </a>
+              </li>
+
+              <li>
+                <a
+                  href="#"
+                  className={`block py-2 px-3 md:p-0 ${
+                    activeButton === "contact"
+                      ? "text-blue-700"
+                      : "text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                  }`}
+                  onClick={() => handleButtonClick("contact")}
+                >
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
-      <div className={`${styles.flexCenter} ${styles.fadeIn} text-center`}>
-        <p className={`${styles.heading2}`}>
-          Shahid Buddhijibe Dr. Muhammad Mortaza Medical Centre
-        </p>
+      <div className={`center-text ${styles.heading1}`}>
+        <p>Shahid Buddhijibe Dr. Muhammad Mortaza Medical Centre</p>
       </div>
-
-   
-
-      
+      <div className="flex-grow">{renderContent()}</div>
     </div>
-      
-   
-
   );
 }
 
